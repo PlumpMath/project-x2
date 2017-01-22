91cc1b5fdac87f5fd9443c74d801ac219580a84a
benjkn <benj.kn@gmail.com>
2017-01-22 23:38:19 +0200

    v1.02

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -1,7 +1,36 @@
 app.controller("mainCtrl", ['$scope', function($scope) {
 
-	// console.log('main controller')
-	$scope.myTasks = ['myTask1', 'myTask2', 'myTask3', 'myTask4', 'myTask5', 'myTask6', 'myTask7', 'myTask8']
+
+
+	$scope.myTasks = [
+		{
+			day: 'monday',
+			tasks: ['myTask1', 'myTask2']
+		},
+		{
+			day: 'tuesday',
+			tasks: ['myTask3', 'myTask4']
+		},
+		{
+			day: 'wednesday',
+			tasks: ['myTask5', 'myTask6']
+		},
+		{
+			day: 'thursday',
+			tasks: ['myTask7', 'myTask8']
+		},
+		{
+			day: 'friday',
+			tasks: ['myTask9', 'myTask10']
+		},
+		{
+			day: 'general',
+			tasks: ['x-tra-task1', 'x-tra-task2']
+		}
+	]
+
 	$scope.teamTasks = ['teamTask1', 'teamTask2', 'teamTask3', 'teamTask4', 'teamTask5', 'teamTask6', 'teamTask7', 'teamTask8']
+
+	console.log($scope.myTasks)
 
 }]);
